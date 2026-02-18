c6ef2033ea07eb39b36ba08e0ca21b94d2f94e36
vladward <vladward13@gmail.com>
2026-02-18 18:53:14 +0300

    add global styles, add header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import type {FC} from 'react';
+import {Header} from "@/widgets/Header";
+import './global.scss'
 
 export const App: FC = () => {
     return (
         <div>
-            Webpack App
+            <Header />
         </div>
     );
 };
