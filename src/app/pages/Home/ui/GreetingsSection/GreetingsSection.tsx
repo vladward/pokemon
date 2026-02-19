4e8758ce16656e961a8248a6ac98544d384b4a7e
vladward <vladward13@gmail.com>
2026-02-19 23:49:43 +0300

    add greetings section

---
+++ src/app/pages/Home/ui/GreetingsSection/GreetingsSection.tsx
@@ -0,0 +1,28 @@
+import type { FC } from 'react';
+
+import { MainImage } from '@/shared/assets';
+
+import styles from './GreetingsSection.module.scss';
+
+export const GreetingsSection: FC = () => {
+  return (
+    <div className={styles.greetings}>
+      <div className={styles.greetings__info}>
+        <p className={styles.greetings__info__greetings}>Энциклопедия мира Pokemon</p>
+        <h1 className={styles.greetings__info__title}>
+          Добро пожаловать в мир
+          <span className={styles.greetings__info__title_highlighted}> покемонов</span>
+        </h1>
+        <h2 className={styles.greetings__info__description}>
+          Стань лучшим тренером: исследуй виды, изучай их способности и собирай свою идеальную
+          команду. Твоё приключение начинается здесь!
+        </h2>
+      </div>
+      <img
+        src={MainImage}
+        alt="pokemons_main"
+        className={styles.greetings__image}
+      />
+    </div>
+  );
+};
