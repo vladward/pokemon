f21d8936c3c6cc913bdd9eb051e87ee46f2760d6
vladward <vladward13@gmail.com>
2026-02-20 23:29:45 +0300

    fix webpack scss import config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,66 +1,72 @@
-import {merge} from 'webpack-merge';
+import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
 export default merge(common, {
-    mode: 'production',
-    devtool: 'source-map',
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: 'ts-loader',
-                        options: {
-                            transpileOnly: true,
-                        }
-                    }
-                ]
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                localIdentName: '[hash:base64:8]',
-                                auto: true,
-                                namedExport: false,
-                                exportLocalsConvention: 'camelCase',
-                            },
-                        },
-                    },
-                    'postcss-loader',
-                    "sass-loader",
-                ],
-            },
-        ]
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
-        },
-        minimizer: [
-            '...',
-            new CssMinimizerPlugin(),
+          },
         ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[hash:base64:8]',
+                auto: true,
+                namedExport: false,
+                exportLocalsConvention: 'camelCase',
+              },
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              additionalData: `
+                @use "@/shared/styles/_variables.scss" as *;
+                @use "@/shared/styles/_mixins.scss" as *;
+              `,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
+    minimizer: ['...', new CssMinimizerPlugin()],
+  },
 });
