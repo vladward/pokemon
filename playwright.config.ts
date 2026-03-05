4e0ebe308a64844211dacc7ebe0db044c0839dc2
vladward <vladward13@gmail.com>
2026-03-05 12:50:51 +0300

    fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   reporter: 'html',
   use: {
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   projects: [
@@ -38,8 +38,8 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: `npm run start -- --port ${process.env.PORT || 3000}`,
-    url: process.env.BASE_URL || 'http://localhost:3000',
+    command: 'npm run start',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
