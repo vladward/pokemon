f5d3c7714cb70bbbcce5dc5e31122232fb27b171
vladward <vladward13@gmail.com>
2026-05-31 14:35:50 +0300

    fix asset declarations: use inline import to keep global ambient scope

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -1,26 +1,24 @@
-import type { StaticImageData } from 'next/image';
-
 declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
 
 declare module '*.png' {
-  const content: StaticImageData;
+  const content: import('next/image').StaticImageData;
   export default content;
 }
 
 declare module '*.jpg' {
-  const content: StaticImageData;
+  const content: import('next/image').StaticImageData;
   export default content;
 }
 
 declare module '*.jpeg' {
-  const content: StaticImageData;
+  const content: import('next/image').StaticImageData;
   export default content;
 }
 
 declare module '*.webp' {
-  const content: StaticImageData;
+  const content: import('next/image').StaticImageData;
   export default content;
 }
