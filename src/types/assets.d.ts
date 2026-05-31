3d4f51d2e8d2c46494919b9aec551538a89b8f57
vladward <vladward13@gmail.com>
2026-05-31 14:34:15 +0300

    fix png asset type to StaticImageData

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -1,24 +1,26 @@
+import type { StaticImageData } from 'next/image';
+
 declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
 
 declare module '*.png' {
-  const content: string;
+  const content: StaticImageData;
   export default content;
 }
 
 declare module '*.jpg' {
-  const content: string;
+  const content: StaticImageData;
   export default content;
 }
 
 declare module '*.jpeg' {
-  const content: string;
+  const content: StaticImageData;
   export default content;
 }
 
 declare module '*.webp' {
-  const content: string;
+  const content: StaticImageData;
   export default content;
 }
