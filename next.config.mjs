2222ca67980280d8e0ab4c0a1e009eefefafb1bb
vladward <vladward13@gmail.com>
2026-05-31 21:54:27 +0300

    add i18next deps and update build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/shared/config/i18n/request.ts');
+
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        formats: ['image/avif', 'image/webp'],
-    },
+  reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
