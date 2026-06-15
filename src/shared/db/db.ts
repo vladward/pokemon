9ae8110f9a9a98d4d81daf7e6eacb9b4d5daf37c
vladward <vladward13@gmail.com>
2026-06-15 20:40:37 +0300

    fix db instance condition

--- src/shared/db/db.ts
+++ src/shared/db/db.ts
@@ -3,16 +3,18 @@ import 'server-only';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const createPrismaClient = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
-};
+export const db = new Proxy({} as PrismaClient, {
+  get(_, prop) {
+    if (!globalForPrisma.prisma) {
+      globalForPrisma.prisma = new PrismaClient({
+        datasources: {
+          db: {
+            url: process.env.DATABASE_URL,
+          },
+        },
+      });
+    }
 
-export const db = globalForPrisma.prisma ?? (globalForPrisma.prisma = createPrismaClient());
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+    return Reflect.get(globalForPrisma.prisma, prop);
+  },
+});
